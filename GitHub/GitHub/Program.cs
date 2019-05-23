61091e53d42ba492b5801dbc734477435cb59452
nchinka <nchinka@CHNSIPDT0T713>
2019-05-23 16:42:50 +0530

    Addition of two numbers

---
+++ GitHub/GitHub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 12;
+            Console.WriteLine("the addition of two numbers" + a + b);
+        }
+    }
+}
